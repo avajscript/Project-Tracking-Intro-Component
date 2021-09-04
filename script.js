3926960f237cb6f21b9b233171df12e7fb10166b
Matthew Pierce <lesbianhaircut@outlook.com>
2021-09-03 22:50:51 -0400

    c

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 window.onload = () => {
   // Variables
-  const navbarButton = document.querySelector(".logo");
-
+  const navbarButton = document.querySelector(".navbar-button");
+  const dropdown = document.querySelector(".nav");
   // Functions
-  function toggleDropdown() {}
+  function toggleDropdown() {
+    console.log("x");
+    if (dropdown.classList.contains("hide")) {
+      dropdown.classList.remove("hide");
+    } else {
+      dropdown.classList.add("hide");
+    }
+  }
 
   // Event Listeners
   navbarButton.addEventListener("click", toggleDropdown);
